e35bbbddf20d18b003f6c37b464318fd2125a2a5
Dominic Sabatier <dsabatier@gmail.com>
2026-09-11 11:20:59 -0600

    lesson-04 exercises

--- lesson-04/conditionals.py
+++ lesson-04/conditionals.py
@@ -14,22 +14,40 @@
 # <=
 # >=
 
+# import random
+
+# guessed_number = 101
+# print(guessed_number)
+# random_number = random.randint(0, 100)
+# print(random_number)
+
+# if guessed_number >= 0 and guessed_number <= 100:
+#     print("you guessed inside of the allowed range.")
+
+#     if guessed_number == random_number:
+#         print("you guessed correct :)")
+#     else:
+#         print("you guessed incorrect.")
+
+#     print("1")
+
+# print("2")
+
 import random
-
-guessed_number = 99
-print(guessed_number)
+ 
+guessed_number = input("guess a number from 0-100")
+ 
 random_number = random.randint(0, 100)
-print(random_number)
-if guessed_number == random_number:
-    print("you guessed correct :)")
-elif True < 0 or guessed_number > 100:
-    print("you guessed outside of the allowed range.")
-elif True == True:
-    print("Why are we here?")
-else:
-    print("you guessed incorrect.")
+
+guessed_int = int(guessed_number)
 
 
+if guessed_int >= 0 and guessed_int <= 100:
+    print("you guessed inside the normal range :O")
+ 
+    if guessed_int == random_number:
+        print("you guessed correct :)")
+    else:
+        print("you guessed incorrect :(")
 
-
-    
+my_bool = False
