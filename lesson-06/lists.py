575387356f2b5485d48e99ae564a5c238584e63e
Dominic Sabatier <dsabatier@gmail.com>
2026-09-16 11:11:14 -0600

    demo code

---
+++ lesson-06/lists.py
@@ -0,0 +1,30 @@
+my_first_list = ["A", "B", "C", "D", "E"]
+
+# print(my_first_list)
+
+# index = -1
+
+# print(my_first_list[index])
+
+# my_first_list[0] = "AAA"
+
+# print(my_first_list)
+
+
+variable_one = 5
+variable_two = 10
+
+# print(variable_one)
+# print(variable_two)
+
+# my_first_list[0] = variable_one
+
+variable_one = 100
+
+# print(my_first_list[0])
+
+# print(my_first_list[2:5])
+
+my_first_list[3:len(my_first_list)]
+
+print(my_first_list[5])
